f8f835f9d5f7ca6e005d2115c92f65ff061444a1
Alex Mendez <AMendez4@student.fullsail.edu>
2025-11-29 18:37:56 -0500

    Removing folders

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
   output: 'export',
-  basePath: '/exercise-1-6-nextjs',
-  assetPrefix: '/exercise-1-6-nextjs',
 };
 
 export default nextConfig;
