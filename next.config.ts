db2f3dad6aab7f9586443db814804906c736d722
Alex Mendez <AMendez4@student.fullsail.edu>
2025-11-29 15:55:53 -0500

    Add gh-pages and export configs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  output: 'export',
+  basePath: '/exercise-1-6-nextjs',
+  assetPrefix: '/exercise-1-6-nextjs',
 };
 
 export default nextConfig;
